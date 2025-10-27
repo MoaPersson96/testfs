47a4b79ee2bfcc8a2b8bb21bc9a823d9776cd3c6
Moa <moa.persson@chasacademy.se>
2025-10-27 18:33:26 +0100

    Fixar eslint fortfarande

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  },
+];
