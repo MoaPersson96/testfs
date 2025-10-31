c7d0fc1762dcd156970a0c947c6d4990aeb70fe0
Moa <moa.persson@chasacademy.se>
2025-10-31 16:24:16 +0100

    fixar stavfel

--- routes.js
+++ routes.js
@@ -3,20 +3,19 @@
  * /users:
  *   get:
  *     summary: Hämtar API:ets status och returnerar ett hälsningsmeddelande.
- *    description: Används för att verifiera att API-tjänsten är igång
- *    tags:
- *     - Status
+ *     description: Används för att verifiera att API-tjänsten är igång
+ *     tags:
+ *       - Status
  *     responses:
  *       '200':
  *         description: API-tjänsten är online och fungerar
- *        content:
- *        application/json:
- *         schema:
- *          type: object
- *         properties:
- *         message:
- *          type: string
- *         example: Hello from the API!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello from the API!
  */
-
 router.get('/', controllers.getApiStatus);
